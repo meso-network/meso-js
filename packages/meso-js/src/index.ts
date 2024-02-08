defbbfc3bab9e3da419e44fe0924da93b5fef6c7
Kyle DeTella <kdetella@gmail.com>
2024-02-08 11:47:59 -0600

    feat(@meso-network/meso-js): export createPostMessageBus from library
    This functionality is required for using meso-js inside of the transfer iframe

--- packages/meso-js/src/index.ts
+++ packages/meso-js/src/index.ts
@@ -3,4 +3,5 @@ import { version } from "../package.json";
 export * from "./types";
 export { transfer } from "./transfer";
 export { validateLayout } from "./validateLayout";
+export { createPostMessageBus } from "./createPostMessageBus";
 export const MESO_JS_VERSION = version;
